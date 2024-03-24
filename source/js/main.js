69310598d57fe72427b4ae74b7f3250cdd0b88f3
Danilina Mariya <mariya199828@gmail.com>
2024-03-24 16:13:14 +0700

    Добавляет блок акций

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,7 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
-const socialsLink = document.querySelector('.isDisabled');
+// const socialsLink = document.querySelector('.isDisabled');
 
 // socialsLink.addEventListener('click',(event) => {
 //   event.preventDefault();
