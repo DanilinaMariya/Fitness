6a05326133ab418ceaabd7303636346c86613b28
Danilina Mariya <mariya199828@gmail.com>
2024-04-23 17:34:27 +0700

    Добавляет ветку для  GitHub Pages

--- source/js/link-disabled.js
+++
@@ -1,7 +0,0 @@
-const link = document.querySelectorAll('a');
-
-link.forEach((el) => {
-  if (el.classList.contains('is-disabled')) {
-    el.preventDefault();
-  }
-});
--- source/js/main.js
+++
@@ -1,9 +0,0 @@
-// https://swiperjs.com/get-started#installation
-
-import './link-disabled';
-import './price';
-import './form';
-import './faq';
-import './iframe';
-import './slider';
-import './swiper';
